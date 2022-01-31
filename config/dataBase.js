b4177f73726334d0940938dab473ab3b795e8cb2
SATHEESH NAYAKA K A <sathishadipad1998@gmail.com>
2022-01-31 11:58:22 +0530

    Database Cluster setup

--- config/dataBase.js
+++ config/dataBase.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-require("dotenv").congif()
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-    });
-  } catch {}
-};
+const DB="mongodb+srv://satheesh:12345@laundry-service.z8cn2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(DB,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+}).then(()=>{
+  console.log("connection established")
+}).catch(err=>{
+  console.log(err)}) 
